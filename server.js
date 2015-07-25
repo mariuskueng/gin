5110b23d92c62a753d721dc201a3e8648c6be8f8
Marius Küng <maku@mailbox.org>
2015-07-25 14:05:34 +0200

    Implement custom documents with urls

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pad');
 });
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for sharejs
+var options = {
+  db: {type: 'redis'},
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
