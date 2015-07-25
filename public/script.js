7c33bf90ba66ca6208e802bad323bcf996bac341
Marius Küng <maku@mailbox.org>
2015-07-25 12:34:04 +0200

    Added styles and script, updated view

---
+++ public/script.js
@@ -0,0 +1,17 @@
+/* public/script.js */
+
+window.onload = function() {
+    var converter = new showdown.Converter();
+    var pad = document.getElementById('pad');
+    var markdownArea = document.getElementById('markdown');
+
+    var convertTextAreaToMarkdown = function(){
+        var markdownText = pad.value;
+        html = converter.makeHtml(markdownText);
+        markdownArea.innerHTML = html;
+    };
+
+    pad.addEventListener('input', convertTextAreaToMarkdown);
+
+    convertTextAreaToMarkdown();
+};
