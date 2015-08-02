6fe3b733dad3860c7b1fb30d30c31618dc59d1be
Marius Küng <maku@mailbox.org>
2015-08-02 14:42:44 +0200

    Removed default preview toggling

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -96,8 +96,6 @@ onload = function() {
   );
 
   setWindowTitle('Untitled');
-  togglePreview();
-  renderMarkdown();
 
   // re-render Markdown on every CodeMirror change event
   cm.on("change", function(e) {
@@ -159,6 +157,7 @@ function setWindowTitle(title) {
 function togglePreview() {
   editor.classList.toggle('preview-visible');
   preview.classList.toggle('preview-visible');
+  renderMarkdown();
 }
 
 function renderMarkdown() {
