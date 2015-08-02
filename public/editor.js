d8a4269e7b6f6cd3ddddda37bc3266d7f804e1ef
Marius Küng <maku@mailbox.org>
2015-08-02 11:27:34 +0200

    Added menu and file operations

--- public/editor.js
+++ public/editor.js
@@ -1,6 +1,53 @@
+var remote = require('remote');
+var Menu = remote.require('menu');
+var dialog = remote.require('dialog');
+var fs = require('fs');
+
+var editor, cm, menu, file, text;
+
+var menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+          label: 'Open File',
+          accelerator: 'Cmd+O',
+          click: function() {
+            var properties = ['multiSelections', 'createDirectory', 'openFile'];
+            var parentWindow = (process.platform == 'darwin') ? null : BrowserWindow.getFocusedWindow();
+
+            dialog.showOpenDialog(parentWindow, properties, function(f) {
+              console.log("got a file: " + f);
+              file = f[0];
+              readFile();
+            });
+          }
+      },
+      {
+        label: 'Save File',
+        accelerator: 'Cmd+S',
+        click: function() {
+          writeFile();
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Cmd+Q',
+        click: function() { app.quit(); }
+      },
+    ]
+  }
+];
+
+menu = Menu.buildFromTemplate(menuTemplate);
+
+Menu.setApplicationMenu(menu);
+
 onload = function() {
-  editor = CodeMirror(
-    document.getElementById("editor"),
+  editor = document.getElementById("editor");
+
+  cm = CodeMirror(
+    editor,
     {
       mode: {
         name: 'gfm',
@@ -22,3 +69,23 @@ onload = function() {
     }
   );
 };
+
+function readFile() {
+  if (file) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) throw err;
+      text = data;
+      cm.setValue(text);
+      console.log('Read a file.');
+    });
+  }
+}
+
+function writeFile() {
+  if (file) {
+    text = cm.getValue();
+    fs.writeFile(file, cm.getValue(), 'utf8', function() {
+      console.log('Wrote a file.');
+    });
+  }
+}
