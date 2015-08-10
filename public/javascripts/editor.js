8a38aeaa68d612d79840fc346439652b6f78d95e
Marius Küng <maku@mailbox.org>
2015-08-10 22:46:07 +0200

    Added chars count and reading duration

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -328,18 +328,57 @@ function clickLinkEvent(e) {
   shell.openExternal(e.srcElement.href);
 }
 
+function getStatusBarText(value, text) {
+  var statusBarText = value + ' ' + text;
+  if (value > 1 || value < 1) statusBarText += 's';
+  return statusBarText;
+}
+
 function countWords() {
   var statusWords = document.querySelector('.status-words');
   var wordsCount = 0;
 
   if (cm.getValue()) wordsCount = cm.getValue().split(' ').length;
 
-  var text = wordsCount + ' word';
-  if (wordsCount > 1 || wordsCount < 1) text += 's';
+  statusWords.innerHTML = getStatusBarText(wordsCount, 'word');
 
-  statusWords.innerHTML = text;
+  return wordsCount;
+}
+
+function countCharacters() {
+  var statusChars = document.querySelector('.status-chars');
+  var charsCount = 0;
+
+  if (cm.getValue()) charsCount = cm.getValue().length;
+
+  statusChars.innerHTML = getStatusBarText(charsCount, 'characters');
+
+  return charsCount;
+}
+
+function setReadingDuration(wordsCount) {
+  var statusReading = document.querySelector('.status-duration');
+  var timeUnit = 'second';
+  var wpm = 250; // words per minute
+  var time = wordsCount / wpm;
+
+  if (wordsCount >= wpm) {
+    // minutes
+    timeUnit = 'minute';
+  } else {
+    // seconds
+    time = time * 60;
+  }
+
+  time = Math.round(time * 10) / 10;
+
+  statusReading.innerHTML = getStatusBarText(time, timeUnit);
+
+  return time;
 }
 
 function renderStatusValues() {
-  countWords();
+  var wordsCount = countWords();
+  countCharacters();
+  setReadingDuration(wordsCount);
 }
