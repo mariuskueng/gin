69adab7aa01b2e57b0153539111927eab3a17573
Marius Küng <maku@mailbox.org>
2015-08-23 21:22:36 +0200

    Added electron-window package

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var window = require('electron-window');
+var path = require('path');
 var ipc = require('ipc');
 var fs = require('fs');
 
@@ -8,7 +10,6 @@ require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
-var mainWindow = null;
 var settings = null;
 var settingsFile = __dirname + '/public/assets/settings.json';
 
@@ -32,51 +33,49 @@ app.on('ready', function() {
       settings = JSON.parse(data);
     }
 
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-      width: settings.width ? settings.width : 800,
-      height: settings.height ? settings.height : 600,
-      'min-width': 460
-    });
-
-    // and load the index.html of the app.
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-    // Open the devtools.
-    // mainWindow.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null;
-
-      // save current window size to settings
-      fs.writeFile(settingsFile, JSON.stringify(settings), 'utf8', function() {
-        // do something
-      });
-    });
-
-    mainWindow.on('resize', function() {
-      var windowSize = mainWindow.getSize();
-      settings.width = windowSize[0];
-      settings.height = windowSize[1];
-    });
+    // create new file
+    newFile();
   });
 });
 
+// create new file from async call
 ipc.on('new-file', function() {
-  var w = new BrowserWindow({
+  newFile();
+});
+
+function newFile() {
+  // Create the browser window.
+  var w = window.createWindow({
     width: settings.width ? settings.width : 800,
     height: settings.height ? settings.height : 600,
     'min-width': 460
   });
 
-  w.setPosition(
-    BrowserWindow.getFocusedWindow().getPosition()[0] + 25,
-    BrowserWindow.getFocusedWindow().getPosition()[1] + 25
-  );
+  var indexPath = path.resolve(__dirname + '/index.html');
 
-  w.loadUrl('file://' + __dirname + '/index.html');
-});
+  // and load the index.html of the app.
+  w.showUrl(indexPath);
+
+  // Open the devtools.
+  // w.openDevTools();
+
+  // Emitted when the window is closed.
+  w.on('closed', function(e) {
+    e.preventDefault();
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    w = null;
+
+    // save current window size to settings
+    fs.writeFile(settingsFile, JSON.stringify(settings), 'utf8', function() {
+      // do something
+    });
+  });
+
+  w.on('resize', function() {
+    var windowSize = w.getSize();
+    settings.width = windowSize[0];
+    settings.height = windowSize[1];
+  });
+}
