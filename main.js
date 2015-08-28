99cfba2cf3e7f15e9d35467201196d3a944b48a7
Marius Küng <maku@mailbox.org>
2015-08-28 20:56:33 +0200

    Removed unneeded comment

--- main.js
+++ main.js
@@ -23,8 +23,6 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // read settings for window size
-
   // create new file
   newFile();
 });
